2840091515f0a910d85eda2c50fa23856fb7027c
New <chauhanraunaq87@gmail.com>
2019-03-31 23:52:34 +0530

    Changed the entire code

--- script.js
+++ script.js
@@ -1,92 +1,108 @@
-const todos = document.querySelector('.todos');
-const taskValue = document.querySelector('#task-value');
-const taskForm = document.querySelector('.task-form');
+const tasksList = document.querySelector('.todos');
+const addForm = document.forms['add-form'];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
-taskForm.addEventListener('submit', function (e) {
+//Add new task
+function addItem(e) {
 	e.preventDefault();
-	const task = taskValue.value;
-	taskValue.value = '';
-	createTask(task);
-});
-
-function createTask(task) {
-	const taskContainer = document.createElement('div');
-	const para = document.createElement('p');
-	para.textContent = task;
-	taskContainer.appendChild(para);
-	todos.appendChild(taskContainer);
-	
-	const deleteButton = document.createElement('button');
-	deleteButton.innerHTML = 'Delete';
-	deleteButton.setAttribute('class', 'delete-task');
-	taskContainer.appendChild(deleteButton);
-	/*deleteButton.addEventListener('click', function (e) {
-		deleteTask(e);
-	});*/
-	
-	const editButton = document.createElement('button');
- 	editButton.innerHTML = 'Edit';
- 	editButton.setAttribute('class', 'edit-task');
- 	taskContainer.appendChild(editButton);
- 	/*editButton.addEventListener('click', function (e) {
- 		editTask(e);
- 	});*/
+	const item = this.querySelector('input').value;
+	items.push(item);
+	populateList(items, tasksList);
+  	localStorage.setItem('items', JSON.stringify(items));
+  	this.reset();
 }
 
-todos.addEventListener('click', function(e) {
-	if(e.target.classList == "delete-task") {
-		deleteTask(e);
-	} else if(e.target.classList == "edit-task") {
-		editTask(e);
+//Populate the task
+function populateList(items, tasksList) {
+	//console.log('in populateList', items);
+	while(tasksList.querySelector('li') != null) {	/*Empty the list first otherwise it will lead to repeated elements getting
+										created because looping thorugh an array is done everytime populateList is called*/
+ 		//console.log('removed list');
+		tasksList.removeChild(tasksList.querySelector('li'));
 	}
+
+	items.forEach(function(item) {
+		const list = document.createElement('li');
+    	const addLabel = document.createElement('label');
+    	addLabel.textContent = item;
+    	tasksList.appendChild(list);
+    	list.appendChild(addLabel);
+    
+    	const deleteButton = document.createElement('button');
+    	deleteButton.classList.add('delete-button');
+    	deleteButton.textContent = 'Delete';
+    	list.appendChild(deleteButton);
+    
+    	const editButton = document.createElement('button');
+    	editButton.classList.add('edit-button');
+    	editButton.textContent = 'Edit';
+    	list.appendChild(editButton);
+	})
+}
+
+//Delete task
+function deleteItem(e) {
+	//console.log('in delete');
+	tasksList.removeChild(e.target.parentNode);
+  	store();	//Update the list after deleting item and store it to Local Storage
+}
+
+//Edit task
+function editItem(e) {
+	const editList = e.target.parentNode;
+  	//console.log(editList.firstElementChild.nodeName);
+  	if (editList.firstElementChild.nodeName == 'LABEL') {
+  		const editForm = document.createElement('form');
+    	const editInput = document.createElement('input');
+    	editInput.value = editList.firstElementChild.textContent;
+ 
+    	//console.log(editInput);
+    	editInput.setAttribute('type', 'text');
+    	editForm.appendChild(editInput);
+    	editList.replaceChild(editForm, editList.firstElementChild);
+    	editInput.focus();
+  	}
+  
+  	editList.firstElementChild.addEventListener('submit', edited);
+
+}
+
+//Edit and submit
+function edited(e) {
+	e.preventDefault();
+	const editedItem = e.target.firstElementChild.value;
+  	if(editedItem.length == 0) {
+  		deleteItem(e);
+  	}
+	const editLabel = document.createElement('label');
+  	editLabel.textContent = editedItem;
+  	e.target.parentNode.replaceChild(editLabel, e.target);
+  	store();  //Update the list after editing item and store it to Local Storage
+}
+
+//Store the updated list after editing or deleting task
+function store() {
+	const finalItems = tasksList.querySelectorAll('li label');
+  	//console.log(finalItems);
+  	items.length = 0;
+  
+  	Array.from(finalItems).forEach(function(finalItem) {
+    	//console.log(finalItem);
+    	items.push(finalItem.textContent);
+    	//console.log(items);
+  	})
+  
+  	localStorage.setItem('items', JSON.stringify(items));
+}
+
+addForm.addEventListener('submit', addItem);
+
+populateList(items, tasksList);
+
+tasksList.addEventListener('click', function(e) {
+	if(e.target.className == "delete-button") {
+  		deleteItem(e);
+  } else if(e.target.className ==  'edit-button') {
+  		editItem(e);
+  }
 });
-
-function deleteTask(e) {
-	console.log('in delete');
-	const selectedButton = e.target;
-	const parentElement = selectedButton.parentNode;
-	const parentContainer = parentElement.parentNode;
-	parentContainer.removeChild(parentElement);
-}
-
-function editTask(e) {
-	console.log('in edit');
-	const selectedButton = e.target;
-	console.log(selectedButton);
-	const parentElement = selectedButton.parentNode;
-	console.log(parentElement);
-	const firstPara = parentElement.firstChild;
-	console.log(firstPara);
-	const formElement = document.createElement('form');
-	const inputElement = document.createElement('input');
-	inputElement.setAttribute('type', 'text');
-	inputElement.value = firstPara.innerHTML;
-	formElement.appendChild(inputElement);
-	parentElement.replaceChild(formElement, firstPara);
-	inputElement.focus();
-	console.log(formElement);
-	formElement.addEventListener('submit', function (e) {
-		e.preventDefault();
-		const clickedButton = e.target;
-		console.log(clickedButton);
-		const parent = clickedButton.parentNode;
-		console.log(parent);
-		const newPara = document.createElement('p');
-		newPara.innerHTML = inputElement.value;
-		console.log(newPara);
-		parent.replaceChild(newPara, formElement);
-	});
-}
-
-
-//Failed Attempt
-/*const buttons = todos.querySelectorAll('button');
-	const paras = todos.querySelectorAll('p');
-	console.log(buttons);
-	for(let i=0; i<buttons.length; i++) {
-	buttons[i].addEventListener('click', function() {
-		console.log('in the loop');
-		paras[i].parentNode.removeChild(paras[i]);
-		buttons[i].parentNode.removeChild(buttons[i]);
-	});
-}*/
